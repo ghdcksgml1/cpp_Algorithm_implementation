0c3a2a555b5bf921c5e3818c51b5c6bcc2c2f81c
홍찬희 <ghdcksgml2@naver.com>
2022-01-19 19:48:11 +0900

    2022.01.19

---
+++ 33_LCS(Longest Common Subsequence).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int LCS[1001][1001];
+
+void init(){
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+}
+
+int main(void){
+  init();
+  string a="";string b="";
+  cin>>a>>b;
+  for(int i=0;i<=a.length();i++){
+    for(int j=0;j<=b.length();j++){
+      if(i==0 || j==0)
+        LCS[i][j] = 0;
+      else if(a[i-1]==b[j-1])
+        LCS[i][j] = LCS[i-1][j-1] + 1;
+      else
+        LCS[i][j] = max(LCS[i-1][j],LCS[i][j-1]);
+    }
+  }
+  cout<<LCS[a.length()][b.length()]<<'\n';
+  return 0;
+}
