c6431b46291afd08122d104580b85ce466949c3c
홍찬희 <ghdcksgml2@naver.com>
2022-02-19 14:46:44 +0900

    Update 23_Euclid.cpp

--- 23_Euclid.cpp
+++ 23_Euclid.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int uclid(int a, int b) {
+int uclid(int a, int b) { // (a > b)
 	if (b == 0) return a;
 	else return uclid(b, a % b);
 }
 
 int main(void) {
-	int n1 = 180;
-	int n2 = 200;
+	int n1 = 200;
+	int n2 = 180;
 	cout << uclid(n1, n2) << '\n';
 }
