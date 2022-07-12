a9286423680e453f46a08dc83af88cea5068dfb1
홍찬희 <ghdcksgml2@naver.com>
2022-07-12 18:41:30 +0900

    Update 34_LIS(Longest Increasing Subsequence).cpp

--- 34_LIS(Longest Increasing Subsequence).cpp
+++ 34_LIS(Longest Increasing Subsequence).cpp
@@ -1,55 +1,58 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <string.h> // memset
 
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1000010
+
 using namespace std;
+typedef long long ll;
 
-int N;
-int arr[1000010];
-int L[1000010];
-int P[1000010];
+int arr[MAX];
+int L[MAX];
+int P[MAX];
+
 vector<int> result;
-
 
 int main(void){
     // 입출력 속도 최적화
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    cin>>N;
+    int N; cin>>N;
 
     int len = 0;
     int idx;
     for(int i=0;i<N;i++){
         cin>>arr[i];
-        int st = 0,en=len,target=arr[i];
-        while(st < en){
-            int mid = (st+en) / 2;
-            if(L[mid] >= target) en = mid;
-            else st = mid+1;
-        }
-        idx = st;
-        L[idx] = arr[i];
-        P[i] = idx;
-        if(idx == len) {
-            len++;
-        }
+        idx = lower_bound(L,L+len,arr[i]) - L;
+        L[idx] = arr[i]; // 현재 숫자정보
+        P[i] = idx; // 몇번재 인덱스인지
+        if(idx == len) len++;
     }
 
     cout<<len<<'\n';
-    len-=1;
-    string res = "";
+    
+    // 인덱스 추적하기
+    len--;
+
     for(int i=N-1;i>=0;i--){
+        if(len < 0) break;
         if(P[i] == len){
             result.push_back(arr[i]);
-            len -= 1;
+            len--;
         }
-        if(len == -1) break;
     }
 
     for(int i=result.size()-1;i>=0;i--){
         cout<<result[i]<<' ';
     }
+
     return 0;
 }
